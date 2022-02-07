57650d360e4f97c6fe651962e499fa89bbf5b458
Matheus Macrineu <matheus.macrineu@connectcom.com.br>
2022-02-07 11:41:31 -0300

    Adicionando rota para empresas

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,13 @@
 <?php
 
 use App\Http\Controllers\{
-    CategoryController
+    CategoryController,
+    CompanyController
 };
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::resource('categories', CategoryController::class);
+Route::apiResource('companies', CompanyController::class);
+Route::apiResource('categories', CategoryController::class);
 
 Route::get('/', function () {
     return response()->json(['message' => 'Success']);
